1ef7cab21c4e0eb4c78279face32254b0c460bf4
Adam Wiwcharuk <adamwiwc@stu.aii.edu>
2015-09-10 17:32:06 -0700

    Start of the portrait system.

---
+++ Assets/Adam'sTestStuf/Scripts/PortraitManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortraitManager : MonoBehaviour {
+
+	public GameObject [] portraits;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Enter(int portrait, int position)
+	{
+		portraits [portrait].SetActive (false);
+
+
+	}
+
+}
---
+++ Assets/Adam'sTestStuf/Scripts/TempStoryManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempStoryManager : MonoBehaviour {
+	PortraitManager manager;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		manager.Enter (0, 0);
+	}
+}
