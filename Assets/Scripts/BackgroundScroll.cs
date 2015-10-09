a741a197390405c46b6bd52ee6d46918581bf5e1
hendrykuo <hendrykuo@stu.aii.edu>
2015-10-09 12:37:04 -0700

    added not working scroll script

---
+++ Assets/Scripts/BackgroundScroll.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class BackgroundScroll : MonoBehaviour {
+
+	public float leftTreshold;
+	public float rightTreshold;
+	public float speed;
+	private Vector2 position;
+	public Button leftButton;
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+	void OnMouseOver()
+	{
+		print ("yay");
+		Debug.Break();
+	}
+}
