b870eee536d1834fdefd7267763fa0d5bd6e0fea
Jesse Taylor <cloudyice_@hotmail.com>
2015-10-10 14:51:11 -0700

    [Added Background Manager] :octocat:
    Press space to change scene

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -1,21 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BackgroundManager : MonoBehaviour 
 {
-
-    public Sprite CurrentBackground;
+    private Sprite CurrentBackground;
+    private Dictionary<string, string> Backgrounds = new Dictionary<string, string>();
+    private GameObject ThisBackground;
+    private SpriteRenderer BgRenderer; 
 
 	void Start () 
     {
-	   
-	}
-	
-	void Update () 
-    {
-	    if(CurrentBackground.Sprite.Enabled)
-        {
+        // TODO(jesse): Create this from file
+        // Create Dictionary
+        Backgrounds.Add( "test", "Textures/Backgrounds/backstreet_test" );
+        Backgrounds.Add( "test2", "Textures/Backgrounds/backstreet_test2" );
 
-        }
+        // Create GameObject
+        ThisBackground = new GameObject("Background");
+        BgRenderer = ThisBackground.AddComponent<SpriteRenderer>();
+
+        // Set initial background
+        ChangeBackground( "test" );
 	}
+
+    public void ChangeBackground(string BackgroundName)
+    {
+        // Load from \Resources\ folder
+        BgRenderer.sprite = Resources.Load<Sprite>( Backgrounds[BackgroundName] );
+    }
 }
---
+++ Assets/Scripts/Debug/JesseTest.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JesseTest : MonoBehaviour 
+{
+
+    private BackgroundManager BgM;
+
+	// Use this for initialization
+	void Start () 
+    {
+        BgM = gameObject.GetComponent<BackgroundManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            BgM.ChangeBackground("test2");
+        }
+	}
+}
