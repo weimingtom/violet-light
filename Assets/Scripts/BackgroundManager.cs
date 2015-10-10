7e5bc0c94733c499a78b56bc2b590ce1b0ebff6d
Jesse Taylor <cloudyice_@hotmail.com>
2015-10-10 13:27:16 -0700

    [Started BG Manager]

---
+++ Assets/Scripts/BackgroundManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundManager : MonoBehaviour 
+{
+
+    public Sprite CurrentBackground;
+
+	void Start () 
+    {
+	   
+	}
+	
+	void Update () 
+    {
+	    if(CurrentBackground.Sprite.Enabled)
+        {
+
+        }
+	}
+}
