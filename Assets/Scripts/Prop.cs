111f1ae9dd9301f70edb1fbd89f609f34dbaa264
Jesse Taylor <cloudyice_@hotmail.com>
2015-10-24 16:03:20 -0700

    [Prop Manager]
    Use the default prop to make props! Then add them to the prop manager.
    Call propmanager spawn to spawn

---
+++ Assets/Scripts/Prop.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Collider2D))]
+public class Prop : MonoBehaviour {
+
+    public bool IsPickUp;
+    public string Description;
+    public string DialougeCall;
+
+    void OnMouseDown()
+    {
+        if(IsPickUp)
+        {
+            //Add to inventory
+            Debug.Log( "[Prop] Picked the item up!" );
+        }
+	}
+}
---
+++ Assets/Scripts/PropManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PropManager : MonoBehaviour 
+{
+
+    public static PropManager Instance;
+
+    public GameObject[] Props;
+
+    void Awake ()
+    {
+        Instance = this;
+    }
+
+    public void Spawn(string Name, Vector3 Position)
+    {
+        foreach(GameObject CurrentProp in Props)
+        {
+            if(CurrentProp.name == Name)
+            {
+                Debug.Log( "[Prop Manager] Spawned Prop " + Name );
+                Instantiate( CurrentProp, Position, Quaternion.identity);
+                return;
+            }
+        }
+        Debug.Log( "[Prop Manager] Cannot find Prop " + Name );
+    }
+}
