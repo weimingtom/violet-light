6d04cd296ef0ce5d91cb02646a58e50e203ed8e4
Adam Wiwcharuk <adamwiwc@stu.aii.edu>
2015-10-24 17:13:39 -0700

    [Character Manager] Added the beginning of the character manager. Not yet ready for use yet.

---
+++ Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,223 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterManager : MonoBehaviour {
+
+    public enum Positions
+    {
+        Offscreen,
+        Left2,
+        Left1,
+        Centre,
+        Right1,
+        Right2
+    }
+
+    public Dictionary<string, Character> characterList = new Dictionary<string,Character>();
+    
+    public const float defaultEasingDuration = 3.0f;
+    public const float defaultDeltaAlpha = 0.04f; //0.0 - 1.0
+    private float easeDuration;
+    private float deltaAlpha;
+
+
+    //TODO account for case of starting another movement before last has finished. 
+    private string changingPortrait;
+    private Positions positionToGoTo;
+    
+    void Start()
+    {
+        changingPortrait = null;
+    }
+
+	void Update () 
+    {
+        if( changingPortrait != null )
+            if( characterList[changingPortrait].GoToNewPosition( positionToGoTo, deltaAlpha, easeDuration ) )
+            {
+                changingPortrait = null;
+                positionToGoTo = Positions.Offscreen;
+            }
+	}
+
+
+    public void ChangePosition(string character, Positions newPosition = Positions.Offscreen, float fadeSpeed = defaultDeltaAlpha, float easeSpeed = defaultEasingDuration) 
+    {
+
+        changingPortrait = character;
+        positionToGoTo = newPosition;
+        characterList[character].SetForMovement(newPosition);
+        easeDuration = easeSpeed;
+        deltaAlpha = fadeSpeed;
+    }
+
+
+
+    private class Character
+    {
+
+        //May need to change expressions so there is functionality to change position
+        //of the face when the pose changes. This is to save memory and not save
+        //multiples of the same face just slightly moved or rotated.
+
+        private string mName;
+        public GameObject mGObject;
+
+        //list of filepaths to all the poses
+        private Dictionary<string, string> mPoses;
+        private SpriteRenderer mPortrait;
+
+        //list of filepaths to all expressions
+        private Dictionary<string, string> mExpressions;
+        //private GameObject mExpressionGO; //not needed for now.
+        private SpriteRenderer mExpressionRend;
+        private Positions myPosition;
+
+        private List<bool> mFlags;  //will add functionality later if time.
+        private int mAffinity;      //will add functionality later if time.
+
+        public Character()
+        { mAffinity = 0; myPosition = Positions.Offscreen; }
+
+        //TODO: change Initialize to take a filepath to a file for that character, parse the file and add all expressions and stuff from here
+        public void Initialize(string name)
+        {
+            mName = name;
+            mGObject = new GameObject(name);
+            mGObject.transform.position = new Vector3( -20, 0, 0 );
+            mPortrait = mGObject.AddComponent<SpriteRenderer>();
+            mExpressionRend = mGObject.AddComponent<SpriteRenderer>();
+        }
+
+        public void AddPose(string name, string filepath)
+        {
+            mPoses.Add(name, filepath);
+        }
+
+        public void AddExpression(string name, string filepath)
+        {
+            mExpressions.Add(name, filepath);
+        }
+        
+        public void ChangePose(string pose)
+        {
+            mPortrait.sprite = Resources.Load<Sprite>( mPoses[pose] );
+        }
+
+        public void ChangeExpression(string expression)
+        {
+            mExpressionRend.sprite = Resources.Load<Sprite>( mExpressions[expression] );
+        }
+
+        public void SetForMovement(Positions position)
+        {
+            mPortrait.color = new Color( 1f, 1f, 1f, 0f );
+            mExpressionRend.color = new Color( 1f, 1f, 1f, 0f );
+            mGObject.transform.position = FindStartPlace( position );
+        }
+
+        //TODO: change these to be able to adapt to the screen size.
+        virtual private Vector3 FindStartPlace( Positions position )
+        { 
+            Vector3 startPlace;
+             switch(position)
+             {
+                case Positions.Left1:
+                startPlace = new Vector3( -10.0f, 0f, 0f);
+                break;
+
+                case Positions.Left2:
+                startPlace = new Vector3( -6.0f, 0f, 0f);
+                break;
+
+                case Positions.Centre:
+                startPlace = new Vector3( 0.0f, 0f, -5.0f);
+                break;
+
+                case Positions.Right1:
+                startPlace = new Vector3( 6.0f, 0f, 0f);
+                break;
+
+                case Positions.Right2:
+                startPlace = new Vector3( 10.0f, 0f, 0f);
+                break;
+         
+                case Positions.Offscreen:
+                default:
+                startPlace = new Vector3( mPortrait.transform.position.x, mPortrait.transform.position.y, mPortrait.transform.position.z );
+                break;
+             }
+
+             return startPlace;
+        }
+
+        virtual private Vector3 FindEndPlace( Positions position ) 
+        {
+            Vector3 endPlace;
+            switch( position )
+            {
+            case Positions.Left1:
+            endPlace = new Vector3( -6.0f, 0f, 0f );
+            break;
+
+            case Positions.Left2:
+            endPlace = new Vector3( -3.0f, 0f, 0f );
+            break;
+
+            case Positions.Centre:
+            endPlace = new Vector3( 0.0f, 0f, 0.0f );
+            break;
+
+            case Positions.Right1:
+            endPlace = new Vector3( 3.0f, 0f, 0f );
+            break;
+
+            case Positions.Right2:
+            endPlace = new Vector3( 6.0f, 0f, 0f );
+            break;
+
+            case Positions.Offscreen:
+            default:
+            endPlace = new Vector3( mPortrait.transform.position.x, mPortrait.transform.position.y, mPortrait.transform.position.z );
+            break;
+            }
+
+            return endPlace;
+        }
+
+        //returns true when done
+        public bool GoToNewPosition( Positions newPosition, float deltaAlpha, float easeDuration )
+        {
+            float alpha = mPortrait.color.a;
+            Vector3 destination;
+           
+
+           
+
+            if( alpha < 1.0 ) //Still doing Fade and ease
+            {
+                mPortrait.color = new Color( 1f, 1f, 1f, alpha + (deltaAlpha * Time.deltaTime) );
+            }
+
+            //ease out
+            Vector3 newposition = new Vector3( mPortrait.transform.position.x, mPortrait.transform.position.y, 0.0f );
+            newposition.x += (destination.x - mPortrait.transform.position.x) / easeDuration;
+            newposition.y += (destination.y - mPortrait.transform.position.y) / easeDuration;
+            mPortrait.transform.position = newposition;
+
+
+            if(false)
+            {
+                return true;
+            }
+
+
+            return false;
+        }
+
+    }
+
+
+	
+}
