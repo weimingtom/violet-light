bea10074bb5e50d80c62959a7c1588e8d480c215
Adam Wiwcharuk <adamwiwc@stu.aii.edu>
2015-10-30 12:32:17 -0700

    [Character Manager]
    Some more work on the character managers.

--- Assets/Scripts/Debug/JesseTest.cs
+++ Assets/Scripts/Debug/JesseTest.cs
@@ -5,6 +5,7 @@ public class JesseTest : MonoBehaviour
 {
 
     private SceneManager BgM;
+    private CharacterManager CM;
 
 	// Use this for initialization
 	void Start () 
@@ -15,6 +16,28 @@ public class JesseTest : MonoBehaviour
         InteractableManager.Instance.Spawn( "DefaultProp", Pos );
         Pos = new Vector3(-3, 1, -1 );
         InteractableManager.Instance.Spawn( "DefaultDoor", Pos );
-	}
-	
+
+        CM = CharacterManager.Instance;
+
+        CM.characterList.Add( "Violet", new CharacterManager.Character());
+        CM.characterList["Violet"].Initialize("Violet Light");
+        CM.characterList["Violet"].AddPose("neutral", "Textures/Portraits/Tintin");
+	    CM.characterList["Violet"].ChangePose("neutral");
+    }
+
+    void Update()
+    {
+
+        if( Input.anyKeyDown )
+        {
+            CM.ChangePosition("Violet", CharacterManager.Positions.Left1);
+        }
+
+
+    }
+
+   
+
+
+
 }
