0bcaf1f8679876dc9c2b0b13c8ee4e84bd0941e0
Jesse Taylor <cloudyice_@hotmail.com>
2015-11-13 09:52:28 -0800

    [PuzMan]
    Fixed

--- Assets/Scripts/Puzzle/PuzzleManager.cs
+++ Assets/Scripts/Puzzle/PuzzleManager.cs
@@ -7,26 +7,67 @@ public class PuzzleManager : MonoBehaviour
     public static PuzzleManager Instance;
 
     public GameObject[] PuzzleList;
+    public bool[]       CompletedPuzzles;
 
     public GameObject PuzzleUIPrefab;
     private GameObject ParentObject;
     private GameObject PuzzleUI;
 
-    private bool PuzzleActive = false;
+    private bool PuzzleLoaded = false;
     private uint CurrentPuzzle = 0;
 
     void Awake ()
     {
         Instance = this;
         ParentObject = new GameObject("PuzzleParent");
-        GameObject MainCanvas = GameObject.Find("Canvas");
-        PuzzleUI = Instantiate(PuzzleUIPrefab,Vector3.zero,Quaternion.identity) as GameObject;
+    }
+
+    void Update()
+    {
+        if(PuzzleLoaded)
+        {
+            // NOTE(jesse): Main Puzzle Loop
+
+
+        }
+    }
+
+    public void StartPuzzle(uint PuzzleNumber)
+    {
+        if( !PuzzleLoaded )
+        {
+            Spawn( CurrentPuzzle );
+            SpawnUI();
+            PuzzleLoaded = true;
+        }
+    }
+
+    public void EndPuzzle()
+    {
+        if( PuzzleLoaded )
+        {
+            Clear();
+            DestroyUI();
+            PuzzleLoaded = false;
+        }
+    }
+
+    private void SpawnUI()
+    {
+        GameObject MainCanvas = GameObject.Find( "Canvas" );
+        PuzzleUI = Instantiate( PuzzleUIPrefab, Vector3.zero, Quaternion.identity ) as GameObject;
         PuzzleUI.transform.parent = MainCanvas.transform;
         PuzzleUI.transform.localScale = Vector3.one;
         PuzzleUI.transform.localPosition = Vector3.zero;
     }
 
-    public void Spawn(uint PuzzleNumber)
+    private void DestroyUI()
+    {
+        // TODO(jesse): Maybe don't destroy this and just hide it?
+        Destroy( PuzzleUI );
+    }
+
+    private void Spawn(uint PuzzleNumber)
     {
         foreach(GameObject Actable in PuzzleList)
         {
@@ -44,7 +85,7 @@ public class PuzzleManager : MonoBehaviour
         Debug.Log( "[Puzzle Manager] Cannot find puzzle " + PuzzleNumber );
     }
 
-    public void Clear()
+    private void Clear()
     {
         Debug.Log( "[Puzzle Manager] Clearing Puzzle");
         foreach( Transform child in ParentObject.transform )
@@ -56,24 +97,9 @@ public class PuzzleManager : MonoBehaviour
 
     // NOTE(jesse): GETTERS AND SETTERS
 
-    public bool GetPuzzleActive()
-    {
-        return PuzzleActive;
-    }
-
-    public void SetPuzzleActive(bool IsActive)
-    {
-        PuzzleActive = IsActive;
-    }
-
     public uint GetCurrentPuzzle()
     {
         return CurrentPuzzle;
-    }
-
-    public void SetCurrentPuzzle(uint New)
-    {
-        CurrentPuzzle = New;
     }
 }
  
