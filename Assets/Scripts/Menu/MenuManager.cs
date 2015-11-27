304a8b6ca1e2984939fe4eb60c15173fb77f8ce1
Adam Wiwcharuk <adamwiwc@stu.aii.edu>
2015-11-27 12:46:04 -0800

    [Menu]
    Added menu Items
    menu can be opened and closed

--- Assets/Scripts/Menu/MenuManager.cs
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -3,13 +3,49 @@ using System.Collections;
 
 public class MenuManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject menu;
+    private Vector2 hiddenPos;
+    private Vector2 ActivePos;
+    private bool active;
+
+	void Start () 
+    {
+        hiddenPos = new Vector2(0.0f, Screen.height * 2);
+        ActivePos = new Vector2( 0.0f, Screen.height );
+        menu.transform.localPosition = hiddenPos;
+        active = false;
+    }
 	
+
+	void Update () 
+    {
+	    //check if menu button was pressed
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            active = !active;
+            if(active)
+                OpenMenu();
+            else
+                CloseMenu();
+        }
+
+        //if menu is open test for button presses 
+        if( active )
+        { 
+        
+        
+        }
+        
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private void OpenMenu()
+    {
+        menu.transform.localPosition = ActivePos;
+    }
+
+    private void CloseMenu()
+    {
+        menu.transform.localPosition = hiddenPos;
+    }
 }
+
