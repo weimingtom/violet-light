c06391743afc494e6f6ab95441dcd878c92ed1cd
Hendry Kuo <hendrykuo@stu.aii.edu>
2015-11-26 23:08:24 -0800

    [Row Script]
    Added on off function
    Check how to swap sprite function tomorrow!

--- Assets/Scripts/Puzzle/Puzzle 1/Row.cs
+++ Assets/Scripts/Puzzle/Puzzle 1/Row.cs
@@ -4,12 +4,45 @@ using System.Collections;
 
 public class Row : MonoBehaviour 
 {
+	public SpriteRenderer[] red;
+	public SpriteRenderer[] blue;
+	public SpriteRenderer[] green;
+	public SpriteRenderer[] yellow;
+
 	public GameObject[] redRow; 	
 	public GameObject[] blueRow;
 	public GameObject[] greenRow;
-	public GameObject[] orangeRow;
+	public GameObject[] yellowRow;
 	void Start()
 	{
 
+	}
+	void SetRedRowActive(bool status)
+	{
+		for(int i = 0; i < redRow.Length; i++)
+		{
+			redRow[i].SetActive(status);
+		}
+	}
+	void SetBlueRowActive(bool status)
+	{
+		for(int i = 0; i < blueRow.Length; i++)
+		{
+			blueRow[i].SetActive(status);
+		}
+	}
+	void SetGreenRowActive(bool status)
+	{
+		for(int i = 0; i < greenRow.Length; i++)
+		{
+			greenRow[i].SetActive(status);
+		}
+	}
+	void SetYellowRowActive(bool status)
+	{
+		for(int i = 0; i < yellowRow.Length; i++)
+		{
+			greenRow[i].SetActive(status);
+		}
 	}
 }
