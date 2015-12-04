2ecbc0a6f48d3ebde61deeb06a6b559c981298c4
Jesse Taylor <cloudyice_@hotmail.com>
2015-12-04 09:22:07 -0800

    [Puzzle 01]
    Updated Abstractness
    Now we will work on gameplay integration

--- Assets/Scripts/Puzzle/Puzzle 1/Puzzle01.cs
+++ Assets/Scripts/Puzzle/Puzzle 1/Puzzle01.cs
@@ -2,7 +2,29 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class Puzzle01 : MonoBehaviour
+public class Puzzle01 : Puzzle
 {
-	
+    // Get Agent Scripts from these?
+    public GameObject AgentA;
+    public GameObject AgentB;
+
+    public override void Initalize()
+    {
+        // set up witches
+    }
+    public override bool IsSolved()
+    {
+        // if witches
+        return false;
+    }
+    public override void Reset()
+    {
+        // Reset Witches
+    }
+    void Update()
+    {
+        //Run witches logic
+        //Check if they both reached the end, 
+        //if they did either reset or change solved to true
+    }
 }
---
+++ Assets/Scripts/Puzzle/Puzzle.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Puzzle : MonoBehaviour {
+
+    public abstract void Initalize();
+    public abstract bool IsSolved();
+    public abstract void Reset();
+
+}
