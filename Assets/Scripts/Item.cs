221f367cc9f455abdb243df8209cd8282dc480f1
Hendry Kuo <hendrykuo@stu.aii.edu>
2015-12-04 12:30:33 -0800

    [Item]
    - added item class

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item
+{
+    private string name;
+    private Texture2D myTexture;
+    private Vector2 position;
+    bool collected;
+    void Awake()
+    {
+        name = "\0";
+    }
+    void SetName(string _nm)
+    {
+        name = _nm;
+    }
+    void SetTexture(string _add)
+    {
+        myTexture = Resources.Load(_add) as Texture2D;
+    }
+    void SetPosition(Vector2 pos )
+    {
+        position = pos;
+    }
+    public void InitializeItem(string _name, string _address, bool _flag)
+    {
+        SetName( _name );
+        SetTexture( _address);
+        SetCollected( _flag );
+    }
+    public void SetCollected(bool _flag)
+    {
+        collected = _flag;
+    }
+    public bool GetCollected()
+    {
+        return collected;
+    }
+    Vector2 GetPosition()
+    {
+        return position;
+    }
+}
---
+++ Assets/Scripts/ItemInventory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ItemInventory : MonoBehaviour
+{
+    Dictionary<string, Item> itemDictionary;
+    void Initialize()
+    {
+        
+    }
+    void AddItem(string idname, string name, string address)
+    {
+        Item temporary = new Item();
+        temporary.InitializeItem(name, address, false);
+        itemDictionary.Add(idname, temporary);
+    }
+    void GetItem( int index )
+    {
+        
+    }
+}
