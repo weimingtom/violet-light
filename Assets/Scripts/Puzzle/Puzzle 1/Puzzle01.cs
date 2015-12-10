572314393c25acdc02cc572410fd036ab08c1199
Hendry Kuo <310079@AI911LAB245P14.AIV.EDMC.STU>
2015-12-10 00:44:27 -0800

    [rawr]

--- Assets/Scripts/Puzzle/Puzzle 1/Puzzle01.cs
+++ Assets/Scripts/Puzzle/Puzzle 1/Puzzle01.cs
@@ -4,44 +4,59 @@ using System.Collections;
 
 public class Puzzle01 : Puzzle
 {
-    // Get Agent Scripts from these?
-    public GameObject AgentA;
-    public GameObject AgentB;
-    public GameObject Rows;
-
-    PuzzleStatus puzzleStatus;
-
-    public override void Initalize()
-    {
-        puzzleStatus = PuzzleStatus.Running;
-    }
-
-    public override PuzzleStatus GetStatus()
-    {
-        if (   AgentA.GetComponent<Agent>().GetWinStatus()
-            && AgentB.GetComponent<Agent>().GetWinStatus() )
-        {
-            puzzleStatus = PuzzleStatus.Win;
-        }
-        else if( AgentA.GetComponent<Agent>().GetLostStatus()
-               ||AgentB.GetComponent<Agent>().GetLostStatus() )
-        {
-            puzzleStatus = PuzzleStatus.Lose;
-        }
-
-        return puzzleStatus;
-    }
-
-    public override void Reset()
-    {
-        AgentA.GetComponent<Agent>().Reset();
-        AgentB.GetComponent<Agent>().Reset();
-        Rows.GetComponent<Row>().    Reset();
-    }
-
-    public override void Submit()
-    {
-        AgentA.GetComponent<Agent>().Submit();
-        AgentB.GetComponent<Agent>().Submit();
-    }
+	// Get Agent Scripts from these?
+	public GameObject AgentA;
+	public GameObject AgentB;
+	public GameObject Rows;
+	
+	PuzzleStatus puzzleStatus;
+	
+	public override void Initalize()
+	{
+		puzzleStatus = PuzzleStatus.Running;
+		//Agent1.instance.Reset ();
+		//Agent2.instance.Reset ();
+	}
+	
+	public override PuzzleStatus GetStatus()
+	{
+		if (Agent1.instance.GetWinStatus () && Agent2.instance.GetWinStatus ())
+		{
+			puzzleStatus = PuzzleStatus.Win;
+		}
+		else if(Agent1.instance.GetLostStatus() || Agent2.instance.GetLostStatus())
+		{
+			puzzleStatus = PuzzleStatus.Lose;
+		}
+		return puzzleStatus;
+		//        if (   AgentA.GetComponent<Agent>().GetWinStatus()
+		//            && AgentB.GetComponent<Agent>().GetWinStatus() )
+		//        {
+		//            puzzleStatus = PuzzleStatus.Win;
+		//        }
+		//        else if( AgentA.GetComponent<Agent>().GetLostStatus()
+		//               ||AgentB.GetComponent<Agent>().GetLostStatus() )
+		//        {
+		//            puzzleStatus = PuzzleStatus.Lose;
+		//        }
+		//
+		//        return puzzleStatus;
+	}
+	
+	public override void Reset()
+	{
+		Agent1.instance.Reset ();
+		Agent2.instance.Reset ();
+		//AgentA.GetComponent<Agent>().Reset();
+		//AgentB.GetComponent<Agent>().Reset();
+		Rows.GetComponent<Row>().    Reset();
+	}
+	
+	public override void Submit()
+	{
+		Agent1.instance.Submit ();
+		Agent2.instance.Submit ();
+		//AgentA.GetComponent<Agent>().Submit();
+		//AgentB.GetComponent<Agent>().Submit();
+	}
 }
