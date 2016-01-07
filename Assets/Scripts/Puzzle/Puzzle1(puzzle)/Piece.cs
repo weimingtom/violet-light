0ace26758185979ed3d63f59822c5a142c07ad12
Adam Wiwcharuk <adamwiwc@stu.aii.edu>
2016-01-07 12:36:20 -0800

    [Puzzle 1] :turtle:
    started work on puzzle 1. The old puzzle (Hendrys test puzzle) has been
    renamed to puzzle 0.

---
+++ Assets/Scripts/Puzzle/Puzzle1(puzzle)/Piece.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piece : MonoBehaviour {
+
+
+
+	void Start () 
+    {
+        this.transform.position = new Vector2(Random.Range(0, 10), Random.Range(0,10));
+        this.transform.Rotate( 0, 0, Random.Range( 0, 360 ) );
+	
+	}
+
+	void Update () 
+    {
+        //test for click
+        if( Input.GetMouseButton(0) )
+        {
+            this.transform.position = Input.mousePosition;
+            
+        }
+	
+	}
+}
---
+++ Assets/Scripts/Puzzle/Puzzle1(puzzle)/Puzzle1.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Puzzle1 : MonoBehaviour {
+
+    public int height = 4;
+    public int width = 3;
+
+    public List<GameObject> pieceList = new List<GameObject>();
+
+	void Start () 
+    {
+        int numPieces = height * width;
+        
+
+        for(int i = 0; i < numPieces; ++i)
+        {
+            //create the puzzle peices from scratch.
+            GameObject puzzlePiece = new GameObject("PuzzlePiece" + i);
+            puzzlePiece.AddComponent<SpriteRenderer>();
+            puzzlePiece.AddComponent<Piece>();
+            pieceList.Add( puzzlePiece );
+            pieceList[i].GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>( "Textures/Puzzle/01/TestPuzzlePiece" );
+            pieceList[i].transform.parent = this.transform;
+
+            //set who they connect to.
+
+        }
+        
+
+
+	
+	}
+	
+	void Update () 
+    {
+        //check for completeion
+
+	
+	}
+}
