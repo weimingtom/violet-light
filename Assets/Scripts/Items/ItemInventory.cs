cf21c4f4f6448b4299a237bdba65d8352d0cffc0
Hendry Kuo <hendrykuo@stu.aii.edu>
2016-01-25 18:54:01 -0800

    [Inventory Stuff]
    - inventory manager now will update the inventory icon and name every
    time player pick up any item
    - added pickup item prefab example
    - Item prefab send signal to item manager to add item

--- Assets/Scripts/Items/ItemInventory.cs
+++ Assets/Scripts/Items/ItemInventory.cs
@@ -6,17 +6,17 @@ public class ItemInventory : MonoBehaviour {
     public static ItemInventory Instance;
     private int size = 0;
     Image mainImageHolder;
-    Text mainTextHolder;
-    //private List<Transform> buttonHolder;
+    Text[] mainTextHolder = new Text[2];
     Button[] buttons;
     void Awake()
 	{
 		Instance = this;
-        buttons = this.GetComponentsInChildren<Button>();
+        
         Initialize();
 	}
     void Initialize()
     {
+		int index = 0;
         for( int i = 0; i < this.transform.childCount; i++ )
         {
             if( this.gameObject.transform.GetChild( i ).tag == "inventory_item_sprites" )
@@ -26,11 +26,20 @@ public class ItemInventory : MonoBehaviour {
             }
             else if( this.gameObject.transform.GetChild( i ).tag == "inventory_item_text" )
             {
-                mainTextHolder = this.gameObject.transform.GetChild( i ).gameObject.GetComponent<Text>();
-                mainTextHolder.text = "No Item Selected\n\nPlease Select Item From Inventory";
+				mainTextHolder[index] = this.gameObject.transform.GetChild( i ).gameObject.GetComponent<Text>();
+				if(index == 0)
+				{
+					mainTextHolder[index].text = "No Item";
+					index++;
+				}
+				else
+				{
+					mainTextHolder[index].text = "~Empty Content~";
+				}
             }
         }
-        buttons[0].onClick.AddListener(() => ClickedButton_0());
+		buttons = this.GetComponentsInChildren<Button>();
+        buttons[0].onClick.AddListener( () => ClickedButton_0() );
         buttons[1].onClick.AddListener( () => ClickedButton_1() );
         buttons[2].onClick.AddListener( () => ClickedButton_2() );
         buttons[3].onClick.AddListener( () => ClickedButton_3() );
@@ -38,50 +47,59 @@ public class ItemInventory : MonoBehaviour {
         buttons[5].onClick.AddListener( () => ClickedButton_5() );
         buttons[6].onClick.AddListener( () => ClickedButton_6() );
     }
-    
     void ClickedButton_0()
     {
-        mainImageHolder.sprite = buttons[0].image.sprite;
-        mainTextHolder.text = ItemManager.Instance.GetDescriptions(buttons[0].GetComponentInChildren<Text>().text);
+		SetMainImage (0);
         Debug.Log("0");
     }
     void ClickedButton_1()
     {
-        mainImageHolder.sprite = buttons[1].image.sprite;
-        mainTextHolder.text = buttons[1].GetComponentInChildren<Text>().text;
+		SetMainImage (1);
         Debug.Log( "1" );
     }
     void ClickedButton_2()
     {
-        mainImageHolder.sprite = buttons[2].image.sprite;
-        mainTextHolder.text = buttons[2].GetComponentInChildren<Text>().text;
+		SetMainImage (2);
         Debug.Log( "2" );
     }
     void ClickedButton_3()
     {
-        mainImageHolder.sprite = buttons[3].image.sprite;
-        mainTextHolder.text = buttons[3].GetComponentInChildren<Text>().text;
+		SetMainImage (3);
         Debug.Log( "3" );
     }
     void ClickedButton_4()
     {
-        mainImageHolder.sprite = buttons[4].image.sprite;
-        mainTextHolder.text = buttons[4].GetComponentInChildren<Text>().text;
+		SetMainImage (4);
         Debug.Log( "4" );
     }
     void ClickedButton_5()
     {
-        mainImageHolder.sprite = buttons[5].image.sprite;
-        mainTextHolder.text = buttons[5].GetComponentInChildren<Text>().text;
+		SetMainImage (5);
         Debug.Log( "5" );
     }
     void ClickedButton_6()
     {
-        mainImageHolder.sprite = buttons[6].image.sprite;
-        mainTextHolder.text = buttons[6].GetComponentInChildren<Text>().text;
+		SetMainImage (6);
         Debug.Log( "6" );
     }
-
+	void SetMainImage(int buttonIndex)
+	{
+		mainImageHolder.sprite = buttons[buttonIndex].image.sprite;
+		mainTextHolder[0].text = buttons[buttonIndex].GetComponentInChildren<Text>().text;
+		mainTextHolder[1].text = ItemManager.Instance.GetDescriptions (buttons[buttonIndex].GetComponentInChildren<Text>().text);
+	}
+	public void ResetButton()
+	{
+		foreach (Button button in buttons) 
+		{
+			button.image.sprite = Resources.Load<Sprite>( "Textures/Item/no_item" );
+			button.GetComponentInChildren<Text>().text = "empty";
+		}
+	}
+	public int GetInventorySize()
+	{
+		return buttons.Length;
+	}
     public void SetItemToInventory( Sprite texture, string name, int index )
     {
         buttons[index].image.enabled = true;
---
+++ Assets/Scripts/Items/ItemPrefab.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPrefab : MonoBehaviour
+{
+	public string name = "bloody_knife";
+
+	void Update()
+	{
+		if( Input.GetMouseButtonUp( 0 ) )
+		{
+			Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Collider2D hitCollider = Physics2D.OverlapPoint(mousePosition);
+			if( hitCollider )
+			{
+				ItemManager.Instance.AddItem(name);
+				Debug.Log(name);
+				Destroy(this.gameObject);
+			}
+		}
+	}
+}
