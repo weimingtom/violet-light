c934550a55f240218d034cbed9fe1e6b10f0c4e2
Adam Wiwcharuk <adamwiwc@stu.aii.edu>
2016-02-02 12:33:36 -0800

    [Save load]

---
+++ Assets/SaveLoad/SaveLoad.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using System.IO;
+
+public class SaveLoad{
+
+	
+}
