21c4d0f2bb6a196751c1bed6db872e46255b95e2
Adam Wiwcharuk <adamwiwc@stu.aii.edu>
2016-02-11 11:12:46 -0800

    [Linear Interpolation]
    Pulled the linear interpolation out of the character manager and into
    it's own script.

---
+++ Assets/Scripts/GraphicalEffects/Blink.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+private class BlinkingItem
+{
+    public BlinkingItem( GameObject obj, float duration, float life, float dead)
+    {
+        myObject = obj;
+        blinkDuration = duration;
+        lifetime = life;
+        if( dead < 0.0f )
+            deadtime = duration;
+        else
+            deadtime = dead;
+        timeSinceLastBlink = Time.time;
+        timeCreated = Time.time;
+    }
+
+    public bool UpdateItem()
+    {
+
+        return true;
+    }
+
+
+    private GameObject myObject;
+    private float blinkDuration; //time between blinks, if deadtime is < 0.0f: deadtime is equal to duration
+    private float deadtime; //used only if the duration it is visible is inequivelent to the duration the item is invisible
+    private float lifetime; //item will stop blinking after this time (< 0.0f is unlimited time)
+
+    private float timeSinceLastBlink;
+    private float timeCreated;
+    
+
+}
+
+public class Blink : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+}
---
+++ Assets/Scripts/GraphicalEffects/LinearInterpolation.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+private class ItemToInterpolate
+{
+    public ItemToInterpolate( GameObject obj, Vector3 pos, float ease)
+    {
+        myObject = obj;
+        finalPos = pos;
+        easeDuration = ease;
+    }
+
+    public bool UpdatePosition()
+    {
+        Vector3 newCords = new Vector3( myObject.transform.position.x, myObject.transform.position.y, myObject.transform.position.z );
+        newCords.x += (finalPos.x - myObject.transform.position.x) / easeDuration;
+        newCords.y += (finalPos.y - myObject.transform.position.y) / easeDuration;
+
+        myObject.transform.position = newCords;
+
+        return myObject.transform.position == finalPos;
+    }
+
+
+    private GameObject myObject;
+    private Vector3 finalPos;
+    private float easeDuration;
+}
+
+public class LinearInterpolation : MonoBehaviour 
+{
+
+    private List<ItemToInterpolate> interpolateList = new List<ItemToInterpolate>();
+
+    public void Interpolate(GameObject item, Vector3 newPos, float easeDuration = 2.0f)
+    {
+        interpolateList.Add( new ItemToInterpolate( item, newPos, easeDuration ) );
+    }
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        for( int i = 0; i < interpolateList.Count; ++i )
+        {
+            if( interpolateList[i].UpdatePosition() )
+            {
+                interpolateList.Remove( interpolateList[i] );
+                break;
+            }
+        
+
+        }
+	
+	}
+}
