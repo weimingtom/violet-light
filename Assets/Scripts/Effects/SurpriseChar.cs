827248d01d6b6ddcad4efba6b655a648a702eabf
Adam Wiwcharuk <adamwiwc@stu.aii.edu>
2016-03-01 12:50:52 -0800

    [character surprise]

---
+++ Assets/Scripts/Effects/SurpriseChar.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SurpriseChar : MonoBehaviour {
+
+    private string characterName;
+
+    private float stretchValue = 0.2f;
+    private float rotateDeg = -15.0f;
+
+
+
+	// Use this for initialization
+	void Init (string charName) 
+    {
+        characterName = charName;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+
+	}
+}
