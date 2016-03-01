31a6cc056c0b13c8838896de56f42c2c91351f95
Jesse Taylor <cloudyice_@hotmail.com>
2016-03-01 00:21:51 -0800

    [Cutscene Update]
    Made it all move and look pretty

---
+++ Assets/Scripts/Cutscenes/CutsceneInterpolateSprite.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneInterpolateSprite : MonoBehaviour 
+{
+
+    public Vector3 endPosition;
+    private Vector3 startPosition;
+    public float speed;
+    private float startTime;
+    private float journeyLength;
+
+    private bool started;
+
+    public bool startOnAwake = false;
+
+    void Start()
+    {
+
+        if(startOnAwake)
+        {
+            StartPan();
+        }
+    }
+
+    public void StartPan()
+    {
+        startPosition = this.transform.position;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance( startPosition, endPosition );
+        started = true;
+    }
+    void Update()
+    {
+        if( started )
+        {
+            float distCovered = (Time.time - startTime) * speed;
+            float fracJourney = distCovered / journeyLength;
+            this.transform.position = Vector3.Lerp( startPosition, endPosition, fracJourney );
+        }
+    }
+}
