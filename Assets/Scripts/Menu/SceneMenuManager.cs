62df188b9c3c5f8635820e7e1a8a34dee5c0dab2
Hendry Kuo <hendrykuo@stu.aii.edu>
2016-03-12 02:05:46 -0800

    prefab update

--- Assets/Scripts/Menu/SceneMenuManager.cs
+++ Assets/Scripts/Menu/SceneMenuManager.cs
@@ -1,25 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SceneMenuManager : MonoBehaviour
 {
     static public SceneMenuManager instance;
-
+    public Button[] mainButton;
+    public Button backButton;
+    string characterOnScene = "";
     void Awake()
     {
         instance = this;
     }
-
+    void Start()
+    {
+        foreach( Button btn in mainButton )
+        {
+            btn.transform.gameObject.SetActive(false);
+        }
+        backButton.transform.gameObject.SetActive( false );
+    }
 	// Use this for initialization
 	public void EnteredNewScene()
     {
+        characterOnScene = SceneManager.Instance.GetChar();
+        if( characterOnScene != "" )
+        {
+            foreach( Button btn in mainButton )
+            {
+                btn.transform.gameObject.SetActive( true );
+            }
+        }
+        else
+        {
+            
+        }
         // Check if there is a character in the scene
             // if yes then show button for PRESENT and for TALK
                 // IF THE TALK BUTTON IS PRESSED RUN:
                     //TalkToCharacterInScene()
+            
                 // if Present button is pressed
                     //MenuManager.instance.OpenItemMenu
             // if no then don't show those buttons
+
         // Check if there are things to click on in the background
             // if yes then show INVESTIGATE button
                 //if investigate button is clicked HIDE THE UI FOR THIS MENU AND SHOW A BACK BUTTON
@@ -28,7 +52,6 @@ public class SceneMenuManager : MonoBehaviour
         
         // Show move button no matter what
     }
-
     void TalkToCharacterInScene()
     {
         Prop Character = GameObject.Find(SceneManager.Instance.GetChar()).GetComponent<Prop>();
